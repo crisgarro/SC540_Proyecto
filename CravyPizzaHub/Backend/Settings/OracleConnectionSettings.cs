d077f290cf86bd2a584c06bdcccf90e1e8daa196
Cristopher Garro <cristophergarro@gmail.com>
2023-10-26 21:58:20 -0600

    Agregado archivo de mapping para la configuracion de la base de datos

---
+++ CravyPizzaHub/Backend/Settings/OracleConnectionSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Settings
+{
+    public class OracleConnectionSettings
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string HostName { get; set; }
+        public string Port { get; set; }
+        public string SID { get; set; }
+    }
+}
