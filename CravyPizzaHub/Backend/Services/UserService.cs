bb4f0ef53c688b39a148f0b40bb1c4c479976311
Cristopher Garro <cristophergarro@gmail.com>
2023-11-11 19:00:09 -0600

    Actualizacion de las paginas de servicio

--- CravyPizzaHub/Backend/Services/UserService.cs
+++ CravyPizzaHub/Backend/Services/UserService.cs
@@ -1,6 +1,5 @@
 ﻿using Backend.Models;
 using Oracle.ManagedDataAccess.Client;
-using Oracle.ManagedDataAccess.Types;
 using System.Data;
 
 namespace Backend.Services
@@ -15,9 +14,7 @@ namespace Backend.Services
 
                 await using (OracleCommand command = new OracleCommand("GetAllUsers", manager.GetConnection()))
                 {
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
-                    
-                    //Output Parameters
+                    command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.Add("p_users", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
 
                     await using (OracleDataReader reader = command.ExecuteReader())
@@ -33,7 +30,7 @@ namespace Backend.Services
                                 FirstName = reader["FirstName"].ToString(),
                                 LastName = reader["LastName"].ToString(),
                                 Address = reader["Address"].ToString(),
-                                Phone = reader["Phone"].ToString()
+                                Phone = reader["Phone"].ToString()                                
                             };
 
                             users.Add(user);
@@ -51,18 +48,16 @@ namespace Backend.Services
             {
                 await using (OracleCommand command = new OracleCommand("InsertUser", manager.GetConnection()))
                 {
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandType = CommandType.StoredProcedure;
 
-                    // Input Parameters
                     command.Parameters.Add("p_username", OracleDbType.Varchar2).Value = newUser.Username;
                     command.Parameters.Add("p_password", OracleDbType.Varchar2).Value = newUser.Password;
                     command.Parameters.Add("p_email", OracleDbType.Varchar2).Value = newUser.Email;
                     command.Parameters.Add("p_firstName", OracleDbType.Varchar2).Value = newUser.FirstName;
                     command.Parameters.Add("p_lastName", OracleDbType.Varchar2).Value = newUser.LastName;
                     command.Parameters.Add("p_address", OracleDbType.Varchar2).Value = newUser.Address;
-                    command.Parameters.Add("p_phone", OracleDbType.Varchar2).Value = newUser.Phone;
+                    command.Parameters.Add("p_phone", OracleDbType.Varchar2).Value = newUser.Phone;                    
 
-                    // Output Parameter
                     command.Parameters.Add("p_userId", OracleDbType.Int32).Direction = ParameterDirection.Output;
 
                     await command.ExecuteNonQueryAsync();
@@ -79,9 +74,8 @@ namespace Backend.Services
             {
                 await using (OracleCommand command = new OracleCommand("UpdateUser", manager.GetConnection()))
                 {
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandType = CommandType.StoredProcedure;
 
-                    // Input Parameters
                     command.Parameters.Add("pUserID", OracleDbType.Int32).Value = updatedUser.UserID;
                     command.Parameters.Add("pUsername", OracleDbType.Varchar2).Value = updatedUser.Username;
                     command.Parameters.Add("pPassword", OracleDbType.Varchar2).Value = updatedUser.Password;
@@ -89,22 +83,17 @@ namespace Backend.Services
                     command.Parameters.Add("pFirstName", OracleDbType.Varchar2).Value = updatedUser.FirstName;
                     command.Parameters.Add("pLastName", OracleDbType.Varchar2).Value = updatedUser.LastName;
                     command.Parameters.Add("pAddress", OracleDbType.Varchar2).Value = updatedUser.Address;
-                    command.Parameters.Add("pPhone", OracleDbType.Varchar2).Value = updatedUser.Phone;
+                    command.Parameters.Add("pPhone", OracleDbType.Varchar2).Value = updatedUser.Phone;                                                                                                    
 
-                    // Output parameter for retrieving the result of the update
                     OracleParameter pIsUpdated = new OracleParameter("pIsUpdated", OracleDbType.Int32);
                     pIsUpdated.Direction = ParameterDirection.Output;
                     command.Parameters.Add(pIsUpdated);
 
                     await command.ExecuteNonQueryAsync();
-
-                    // Check the value of pIsUpdated to determine if the update was successful
                     isUpdated = Convert.ToInt32(pIsUpdated.Value.ToString());
-
-                    // If pIsUpdated is 1, the update was successful, and you can return the updatedUser
-                    // Otherwise, return null or handle the failure as needed                    
                 }
             }
+
             if (isUpdated == 1)
             {
                 return updatedUser;
@@ -114,7 +103,5 @@ namespace Backend.Services
                 return null; // or handle the failure in your application
             }
         }
-
-
     }
 }
