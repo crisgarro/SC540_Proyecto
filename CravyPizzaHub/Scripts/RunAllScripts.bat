a0b797e33bbba2b257d956c67261fd650068a916
Cristopher Garro <cristophergarro@gmail.com>
2023-11-16 01:10:33 -0600

    Archivo de Correr script actualizado para que ignore el full script

--- CravyPizzaHub/Scripts/RunAllScripts.bat
+++ CravyPizzaHub/Scripts/RunAllScripts.bat
@@ -5,7 +5,7 @@ set USERNAME=CravyDev
 set PASSWORD=abcd1234
 set SID=ORCLCDB
 set SQLPLUS_PATH=C:\git\Oracle\sqldeveloper-23.1.0.097.1607-x64\sqldeveloper\sqldeveloper\bin\sql.exe
-set EXCLUDED_SCRIPT=01-Script_Inicial.sql
+set EXCLUDED_SCRIPT=01-Script_Inicial.sql FullScript.sql
 REM Para no correr los script sin recrear las tablas
 REM EXCLUDED_SCRIPT=01-Script_Inicial.sql 02-Script_Creacion_DB.sql
 
