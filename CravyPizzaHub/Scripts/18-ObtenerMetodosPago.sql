274f5c9e171b2884a32a2f1ee976a6b7d119b6d0
Cristopher Garro <cristophergarro@gmail.com>
2023-11-16 01:25:25 -0600

    Arreglados los problemas del orden de los scripts

--- CravyPizzaHub/Scripts/18-ObtenerMetodosPago.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE GetAllPaymentMethods(
-    p_Result OUT SYS_REFCURSOR
-)
-IS
-BEGIN
-    OPEN p_Result FOR
-    SELECT PaymentMethodID, PaymentMethodName, Enabled FROM Payment_Methods;
-END GetAllPaymentMethods;
