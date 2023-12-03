7f441f1602f6febf76deb00e7ead6d8400198ccd
Cristopher Garro <cristophergarro@gmail.com>
2023-12-03 11:40:41 -0600

    Actualizacion de metodo de pago para permitir filtrar los tipos activos

--- CravyPizzaHub/Scripts/24-ObtenerMetodosPago.sql
+++ CravyPizzaHub/Scripts/24-ObtenerMetodosPago.sql
@@ -1,9 +1,8 @@
-CREATE OR REPLACE PROCEDURE GetAllPaymentMethods(
+create or replace PROCEDURE GetAllPaymentMethods(
     p_Result OUT SYS_REFCURSOR
 )
 IS
 BEGIN
     OPEN p_Result FOR
-    SELECT PaymentMethodID, PaymentMethodName, Enabled FROM Payment_Methods;
+    SELECT PaymentMethodID, PaymentMethodName, Enabled FROM Payment_Methods WHERE Enabled = 1;
 END GetAllPaymentMethods;
-/
