6be178d407b59085ee537212dd86c00fb8ebda57
Cristopher Garro <cristophergarro@gmail.com>
2023-12-07 22:08:08 -0600

    Cambios revertidos a archivos de configuracion

--- CravyPizzaHub/Scripts/RunAllScripts.bat
+++ CravyPizzaHub/Scripts/RunAllScripts.bat
@@ -3,8 +3,8 @@
 set SCRIPTS_FOLDER=.
 set USERNAME=CravyDev
 set PASSWORD=abcd1234
-set SID=xe
-set SQLPLUS_PATH=C:\sqldeveloper\sqldeveloper\sqldeveloper\bin\sql.exe
+set SID=ORCLCDB
+set SQLPLUS_PATH=C:\git\Oracle\sqldeveloper-23.1.0.097.1607-x64\sqldeveloper\sqldeveloper\bin\sql.exe
 set EXCLUDED_SCRIPT=01-Script_Inicial.sql FullScript.sql
 REM Para no correr los script sin recrear las tablas
 REM EXCLUDED_SCRIPT=01-Script_Inicial.sql 02-Script_Creacion_DB.sql
