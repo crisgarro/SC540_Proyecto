72ce936135167dddc9e838fee495b641913311b3
Franklin Gaitán Corea <frgaitan24@gmail.com>
2023-12-12 14:05:45 -0600

    Vista compras con descuentos

---
+++ CravyPizzaHub/Scripts/78-Vista_compras_con_descuentos.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW compras_con_descuentos AS
+SELECT
+    o.ORDERID,
+    o.TOTALAMOUNT,
+    p.PROMOTIONNAME,
+    p.STARTDATE,
+    p.ENDDATE,
+    p.DISCOUNTAMOUNT,
+    u.FIRSTNAME,
+    u.LASTNAME
+FROM
+    orders o
+JOIN
+    applied_promotions ap ON o.ORDERID = ap.ORDERID
+JOIN
+    promotions p ON ap.PROMOTIONID = p.PROMOTIONID
+JOIN
+    users u ON o.USERID = u.USERID;
