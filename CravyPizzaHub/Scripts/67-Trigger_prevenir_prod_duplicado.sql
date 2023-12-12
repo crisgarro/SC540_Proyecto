a7fb82f2bca8f37dd5adc64f3faf93e715ce0305
Franklin Gaitán Corea <frgaitan24@gmail.com>
2023-12-11 22:08:07 -0600

    Trigger prevenir crear productos duplicados

---
+++ CravyPizzaHub/Scripts/67-Trigger_prevenir_prod_duplicado.sql
@@ -0,0 +1,17 @@
+create or replace NONEDITIONABLE TRIGGER prevent_duplicate_product
+BEFORE INSERT ON products
+FOR EACH ROW
+DECLARE
+    existing_product_count NUMBER;
+BEGIN
+    -- Verificar si ya existe un producto con el mismo nombre
+    SELECT COUNT(*)
+    INTO existing_product_count
+    FROM products
+    WHERE UPPER(name) = UPPER(:NEW.name);
+
+    -- Si ya existe un producto con el mismo nombre, lanzar una excepción
+    IF existing_product_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Ya existe un producto con el mismo nombre en la tabla.');
+    END IF;
+END;
