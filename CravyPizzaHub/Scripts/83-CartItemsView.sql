7af74d871a9792a7bdc194a6aaa254bcd085c8d0
Cristopher Garro <cristophergarro@gmail.com>
2023-12-14 17:15:08 -0600

    Actualizacion de Vista de Carrito

--- CravyPizzaHub/Scripts/83-CartItemsView.sql
+++ CravyPizzaHub/Scripts/83-CartItemsView.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE VIEW CartItemsView AS
-SELECT CartItemID, CartID, ProductID, Quantity
-FROM Cart_Items;
+SELECT c.cartid, p.name, ci.quantity, p.description
+FROM Cart_Items ci
+INNER JOIN Cart c on c.cartid = ci.cartid
+INNER JOIN Products p on p.ProductID = ci.ProductID
+INNER JOIN Users u on u.userId = c.userid
 /
